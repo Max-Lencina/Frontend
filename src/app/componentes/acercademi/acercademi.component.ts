a1e09f2c574a15eaef9aa9ea86f24ddfa491c19b
Max-Lencina <maxilencina1985@gmail.com>
2022-09-19 17:20:54 -0300

    commit 29

--- src/app/componentes/acercademi/acercademi.component.ts
+++ src/app/componentes/acercademi/acercademi.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-acercademi',
@@ -8,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercademiComponent implements OnInit {
 
-  
-  constructor() { }
+  miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
-   
+    this.datosPortfolio.ObtenerDatos().subscribe(data => {console.log(data);
+      this.miPortfolio=data;  
+      });
   }
 
  }
